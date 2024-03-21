e9661f0bb4242e98ea8964e1c2c1bcee9eacb556
ChiefLopes <chieflopesekede@gmail.com>
2024-03-21 04:59:15 +0100

    working on the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,15 +7,26 @@ const Navbar = () => {
       <div>
         <Link
           href="/"
-          className="text-sm bg-black rounded-md p-1 font-semibold"
+          className="text-sm bg-black rounded-md p-1 font-semibold flex justify-center items-center"
         >
-          <span className="text-white">Lopes</span>
-          <span className="w-12 h-8 rounded bg-white text-black">.dev</span>
+          <span className="text-white mr-1">Lopes</span>
+          <span className="w-12 h-8 rounded bg-white text-black flex justify-center items-center">
+            .dev
+          </span>
         </Link>
       </div>
 
       {/* RESPONSIVE MENU */}
-      <div>2</div>
+      <div>
+        <button className="w-10 h-6 flex flex-col justify-between">
+          <div className="w-10 h-1 bg-black rounded"></div>
+          <div className="w-10 h-1 bg-black rounded"></div>
+          <div className="w-10 h-1 bg-black rounded"></div>
+              </button>
+              
+
+              {/* MENU LIST */}
+      </div>
     </div>
   );
 };
