d9f98459249521c6a8e89395594f617c8100f221
ChiefLopes <chieflopesekede@gmail.com>
2024-03-21 04:45:08 +0100

    working on the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,21 @@
-import React from "react";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 ">
-      <div>1</div>
+      {/* LOGO  */}
+      <div>
+        <Link
+          href="/"
+          className="text-sm bg-black rounded-md p-1 font-semibold"
+        >
+          <span className="text-white">Lopes</span>
+          <span className="w-12 h-8 rounded bg-white text-black">.dev</span>
+        </Link>
+      </div>
+
+      {/* RESPONSIVE MENU */}
       <div>2</div>
-      <div>3</div>
-      <div>3</div>
     </div>
   );
 };
