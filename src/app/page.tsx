52e9ed2f6f5010c668f42e27787aa95137fe1e50
ChiefLopes <chieflopesekede@gmail.com>
2024-03-22 05:17:50 +0100

    working on the home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,24 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="h-full flex flex-col px-4 sm:px-8 md:px-12 lg:px-20 xl:px-36">
+    <div className="h-full flex flex-col lg:flex-row px-4 gap-4 sm:px-8 md:px-12 lg:px-20 xl:px-36 ">
       {/* IMAGE SECTION */}
-      <div className="h-1/2 relative ">
+      <div className="h-1/2 relative lg:h-full lg:w-1/2 ">
         <Image src="/hero.png" alt="hero" fill className="object-contain" />
       </div>
 
       {/* TEXT SECTION */}
-      <div className="h-1/2 flex flex-col gap-4">
+      <div className="h-1/2 flex flex-col gap-4 lg:gap-8 lg:pt-10 lg:h-full lg:w-1/2">
         {/* TITLE */}
-        <h1 className="font-bold text-xl md:text-3xl">
+        <h1 className="font-bold text-xl md:text-6xl">
           Building Interactive User Interfaces, <br /> Redefining User
           Experience...
         </h1>
 
         {/* DECS. */}
-        <p className="">
+        <p className="md:text-xl">
           🚀 Frontend Web Developer | Passionate about crafting clean codes and
           building Interactive UIs. 💻 Exploring the endless possibilities in
           the world of tech, leveraging the awesome power of the javascript
@@ -25,13 +26,17 @@ export default function Home() {
         </p>
 
         {/* BUTTONS */}
-        <div className="flex gap-4">
-          <button className="p-3 rounded-lg ring-1 ring-black text-white">
-            View My Works
-          </button>
-          <button className="p-3 rounded-lg ring-1 ring-black text-white">
-            Contact Me
-          </button>
+        <div className="flex gap-6 w-full">
+          <Link href="/portfolio">
+            <button className="p-3 bg-black rounded-lg ring-1 ring-black text-white">
+              View My Works
+            </button>
+          </Link>
+          <Link href="/contact">
+            <button className="p-3 rounded-lg ring-1 ring-black text-black">
+              Contact Me
+            </button>
+          </Link>
         </div>
       </div>
     </div>
