7a64058ef2ec7a23db98d24f82791a90e55416eb
ChiefLopes <chieflopesekede@gmail.com>
2024-03-22 22:54:54 +0100

    added framer motion effects to the about page

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import {motion as m} from "framer-motion"
 
 const PortfolioPage = () => {
   return (
-    <div>PortfolioPage</div>
-  )
+    <m.div
+      className="h-full"
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}
+    ></m.div>
+  );
 }
 
 export default PortfolioPage
